b9df559f46368febfc33f519be99c9b8a842f517
Ktodd213 <127579094+Ktodd213@users.noreply.github.com>
2025-01-06 20:51:06 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello
     public static void main(String[] args) 
     {
         System.out.println("Hello, world! From: Java :]");
-        System.out.println("Changes have been made")
+        System.out.println("Changes have been made");
         Java syntax error
     }
 }
