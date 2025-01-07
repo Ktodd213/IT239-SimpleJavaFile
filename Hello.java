45f56a5954dec21d4109d39a56306197673150d0
Ktodd213 <127579094+Ktodd213@users.noreply.github.com>
2025-01-07 09:21:57 -0500

    Update Hello.java
    Java syntax Error

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ public class Hello
     public static void main(String[] args) 
     {
         System.out.println("Hello, world! From: Java :]");
-        System.out.println("Changes have been made");
+        System.out.println("Changes have been made")
+        System.out.println("Java syntax error");
+
     }
+    
 }
