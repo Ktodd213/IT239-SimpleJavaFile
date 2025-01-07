69ba1c02a4428015ce848b990b23afd4eaec9c69
Ktodd213 <127579094+Ktodd213@users.noreply.github.com>
2025-01-06 20:41:11 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello
     public static void main(String[] args) 
     {
         System.out.println("Hello, world! From: Java :]");
-        System.out.println("Changes have been made"
+        System.out.println("Changes have been made")
     }
 }
